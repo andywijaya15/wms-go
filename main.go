795bb3a26e6ed80d09433f632b23bdbfc354c6e4
andywijaya15 <andykevonata@gmail.com>
2025-01-09 16:30:20 +0700

    change main function

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"wms-go/cmd"
@@ -10,22 +9,35 @@ import (
 	"wms-go/routes"
 )
 
+const (
+	SyncAutoPrCommand = "sync-auto-pr"
+)
+
+func handleCommand(command string) {
+	switch command {
+	case SyncAutoPrCommand:
+		cmd.SyncAutoPr()
+	default:
+		log.Fatalf("Unknown command: %v", command)
+	}
+}
+
+func startServer() {
+	config.LoadEnv()
+	models.ConnectDatabase()
+	router := routes.SetupRouter()
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal("Error starting the server: ", err)
+	}
+}
+
 func main() {
+
 	isCommand := len(os.Args) > 1 && os.Args[1] != ""
 	if isCommand {
-		switch os.Args[1] {
-		case "sync-auto-pr":
-			cmd.SyncAutoPr()
-		case "sync-auto-pr1":
-			fmt.Println(2)
-		}
+		handleCommand(os.Args[1])
 	} else {
-		config.LoadEnv()
-		models.ConnectDatabase()
-		router := routes.SetupRouter()
-
-		if err := router.Run(":8080"); err != nil {
-			log.Fatal("Error starting the server: ", err)
-		}
+		startServer()
 	}
 }
